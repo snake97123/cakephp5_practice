6bf68ddd6839246b74878aece5f59ce1de5fe4d4
snake97123 <73522198+snake97123@users.noreply.github.com>
2024-06-25 14:17:35 +0900

    Article modelの作成

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+
+class Article extends Entity
+{
+    protected array $_accessible = [
+        'title' => true,
+        'body' => true,
+        'published' => true,    
+        'created' => true,
+        'modified' => true,
+        'users' => true,
+    ];
+}
